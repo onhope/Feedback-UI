a6f38b98de58ff9087ac4c030c7599917b2b23ac
onhope <chch4934@gmail.com>
2023-10-24 16:39:02 +0900

    Feedback UI

---
+++ index.js
@@ -0,0 +1,33 @@
+const ratingsEl = document.querySelectorAll(".rating");
+const btnEl = document.getElementById("btn");
+const containerEl = document.getElementById("container");
+let selectedRating = "";
+
+
+ratingsEl.forEach((ratingEl) => {
+  ratingEl.addEventListener("click", (event) => {
+    removeAcitive();
+    selectedRating = event.target.innerText || event.target.parentNode.innerText;
+    console.log(event.target.innerText || event.target.parentNode.innerText);
+    event.target.classList.add("active");
+    event.target.parentNode.classList.add("active");
+  });
+});
+
+btnEl.addEventListener("click", () => {
+  if(selectedRating !== "") {
+    containerEl.innerHTML = `
+      <strong>Thank You!</strong>
+      <br>
+      <br>
+      <strong>Feedback : ${selectedRating}</strong>
+      <p>we'll use your feedback to improve our customer support.</p>
+    `;
+  }
+})
+
+function removeAcitive() {
+  ratingsEl.forEach((ratingEl) => {
+    ratingEl.classList.remove("active");
+  })
+}
